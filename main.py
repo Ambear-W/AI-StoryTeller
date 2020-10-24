7c4633bd2f0b4800d2c4cc21b28cb2b5a3de018b
Amber <wells.amber16@gmail.com>
2020-10-24 00:54:04 +0000

    okay so, for some reason he doesn't like to generate real words.  Just a bunch of different strings of letters.  But he's trying his best and I am proud of him

--- main.py
+++ main.py
@@ -1,18 +1,21 @@
+#tensorflor's code that i wanted to use to learn how everything works - aka getting a bases on how everything works
 import tensorflow as tf
 
 import numpy as np
 import os
 import time
 
-#tf.keras.Model.reset_states
+tf.keras.Model.reset_states
 
 path_to_file = tf.keras.utils.get_file('shakespeare.txt', 'https://storage.googleapis.com/download.tensorflow.org/data/shakespeare.txt')
-
 
 # Read, then decode for py2 compat.
 text = open(path_to_file, 'rb').read().decode(encoding='utf-8')
 # length of text is the number of characters in it
 #print('Length of text: {} characters'.format(len(text)))
+
+# Take a look at the first 250 characters in text
+#print(text[:250])
 
 # The unique characters in the file
 vocab = sorted(set(text))
@@ -24,6 +27,14 @@ idx2char = np.array(vocab)
 
 text_as_int = np.array([char2idx[c] for c in text])
 
+#print('{')
+#for char,_ in zip(char2idx, range(20)):
+#    print('  {:4s}: {:3d},'.format(repr(char), char2idx[char]))
+#print('  ...\n}')
+
+# Show how the first 13 characters from the text are mapped to integers
+#print('{} ---- characters mapped to int ---- > {}'.format(repr(text[:13]), text_as_int[:13]))
+
 # The maximum length sentence you want for a single input in characters
 seq_length = 100
 examples_per_epoch = len(text)//(seq_length+1)
@@ -32,19 +43,29 @@ examples_per_epoch = len(text)//(seq_length+1)
 char_dataset = tf.data.Dataset.from_tensor_slices(text_as_int)
 
 #for i in char_dataset.take(5):
-    #print(idx2char[i.numpy()])
+#    print(idx2char[i.numpy()])
 
 sequences = char_dataset.batch(seq_length+1, drop_remainder=True)
 
 #for item in sequences.take(5):
-    #print(repr(''.join(idx2char[item.numpy()])))
-
+#    print(repr(''.join(idx2char[item.numpy()])))
+    
 def split_input_target(chunk):
     input_text = chunk[:-1]
     target_text = chunk[1:]
     return input_text, target_text
 
 dataset = sequences.map(split_input_target)
+
+#for input_example, target_example in  dataset.take(1):
+#    print('Input data: ', repr(''.join(idx2char[input_example.numpy()])))
+#    print('Target data:', repr(''.join(idx2char[target_example.numpy()])))
+
+#for i, (input_idx, target_idx) in enumerate(zip(input_example[:5], target_example[:5])):
+#    print("Step {:4d}".format(i))
+#    print("  input: {} ({:s})".format(input_idx, repr(idx2char[input_idx])))
+#    print("  expected output: {} ({:s})".format(target_idx, repr(idx2char[target_idx])))
+
 
 # Batch size
 BATCH_SIZE = 64
@@ -86,19 +107,17 @@ model = build_model(
     rnn_units=rnn_units,
     batch_size=BATCH_SIZE)
 
-for input_example_batch, target_example_batch in dataset.take(1):
-    example_batch_predictions = model(input_example_batch)
-    #print(example_batch_predictions.shape, "# (batch_size, sequence_length, vocab_size)")
+#for input_example_batch, target_example_batch in dataset.take(1):
+#    example_batch_predictions = model(input_example_batch)
+#    print(example_batch_predictions.shape, "# (batch_size, sequence_length, vocab_size)")
 
-sampled_indices = tf.random.categorical(example_batch_predictions[0], num_samples=1)
-sampled_indices = tf.squeeze(sampled_indices,axis=-1).numpy()
+#model.summary()
+
+#sampled_indices = tf.random.categorical(example_batch_predictions[0], num_samples=1)
+#sampled_indices = tf.squeeze(sampled_indices,axis=-1).numpy()
 
 def loss(labels, logits):
     return tf.keras.losses.sparse_categorical_crossentropy(labels, logits, from_logits=True)
-
-example_batch_loss = loss(target_example_batch, example_batch_predictions)
-#print("Prediction shape: ", example_batch_predictions.shape, " # (batch_size, sequence_length, vocab_size)")
-#print("scalar_loss:      ", example_batch_loss.numpy().mean())
 
 model.compile(optimizer='adam', loss=loss)
 
@@ -111,45 +130,18 @@ checkpoint_callback = tf.keras.callbacks.ModelCheckpoint(
     filepath=checkpoint_prefix,
     save_weights_only=True)
 
-optimizer = tf.keras.optimizers.Adam()
+EPOCHS = 1000
 
-@tf.function
-def train_step(inp, target):
-    with tf.GradientTape() as tape:
-        predictions = model(inp)
-        loss = tf.reduce_mean(
-            tf.keras.losses.sparse_categorical_crossentropy(
-                target, predictions, from_logits=True))
-    grads = tape.gradient(loss, model.trainable_variables)
-    optimizer.apply_gradients(zip(grads, model.trainable_variables))
+#history = model.fit(dataset, epochs=EPOCHS, callbacks=[checkpoint_callback])
 
-    return loss
 
-# Training step
-EPOCHS = 30
+tf.train.latest_checkpoint(checkpoint_dir)
 
-for epoch in range(EPOCHS):
-    start = time.time()
+model = build_model(vocab_size, embedding_dim, rnn_units, batch_size=1)
 
-    # resetting the hidden state at the start of every epoch
-    model.reset_states()
+model.load_weights(tf.train.latest_checkpoint(checkpoint_dir))
 
-    for (batch_n, (inp, target)) in enumerate(dataset):
-        loss = train_step(inp, target)
-
-        if batch_n % 100 == 0:
-            template = 'Epoch {} Batch {} Loss {}'
-            #print(template.format(epoch + 1, batch_n, loss))
-
-    # saving (checkpoint) the model every 5 epochs
-    if (epoch + 1) % 5 == 0:
-        model.save_weights(checkpoint_prefix.format(epoch=epoch))
-
-    #print('Epoch {} Loss {:.4f}'.format(epoch + 1, loss))
-    #print('Time taken for 1 epoch {} sec\n'.format(time.time() - start))
-
-model.save_weights(checkpoint_prefix.format(epoch=epoch))
-
+model.build(tf.TensorShape([1, None]))
 
 def generate_text(model, start_string):
     # Evaluation step (generating text using the learned model)
